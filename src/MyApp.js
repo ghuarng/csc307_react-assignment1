e2ae2193a9e6f0ffa2621fc06d965b14e5aa8a53
gary <gary0928@gmail.com>
2021-03-30 21:33:59 -0700

    Add Table.js

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Table from './Table'
 
 function MyApp(){
     return(
-        <div>
-            <h1>Hello, React!</h1>
+        <div className="container">
+            <Table />
         </div>
     );
 }
