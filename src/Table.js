72a3e4f28fb7c29c321fde39ad28c92b89f999c5
gary <gary0928@gmail.com>
2021-03-31 12:22:23 -0700

    Pass Table body with props

--- src/Table.js
+++ src/Table.js
@@ -10,33 +10,28 @@ function TableHeader() {
         </thead>
     );
 }
-function TableBody(){
+function TableBody(props){
+  const rows = props.characterData.map((row, index) => {
     return(
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
     );
+  }
+  );
+
+  return(
+    <tbody>
+      {rows}
+    </tbody>
+  );
 }
-function Table() {
+function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody />
+        <TableBody characterData={props.characterData} />
       </table>
     );  
 }
