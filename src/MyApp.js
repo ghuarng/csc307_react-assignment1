e32cd342decb018895fea14df230df7ed4d0c5f1
gary <gary0928@gmail.com>
2021-04-07 09:04:40 -0700

    Retrieve user_list - CORS blocked

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp(){
     const [characters, setCharacters] = useState([]);
 
+    useEffect(() => {
+        axios.get('http://localhost:5000/users')
+            .then(res => {
+                setCharacters(res.data.users_list)
+            })
+            .catch(function (error) {
+                //Not handling, only logging
+                console.log(error);
+            });
+    }, [])
     function removeOneCharacter (index) {
         const updated = characters.filter((character, i) => {
             return i !== index
